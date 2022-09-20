ca8689000037462f9db378a877a9ca05bd7e513a
Sam Jones <sam0jones0@gmail.com>
2022-09-20 17:24:41 +0100

    Initial Spring Init setup

---
+++ src/main/java/com/getyourway/GetyourwayApplication.java
@@ -0,0 +1,13 @@
+package com.getyourway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetyourwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetyourwayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/getyourway/GetyourwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getyourway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetyourwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
