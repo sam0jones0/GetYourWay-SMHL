8fe9dc2c366ace7ce43fe0205a98babacbbc7184
Hilary Onoabhagbe <hilary.onoabhagbe@sky.uk>
2022-09-20 18:02:41 +0100

    Project would not build. Made a one line change to allow it to build

--- src/main/java/com/getyourway/GetyourwayApplication.java
+++ src/main/java/com/getyourway/GetyourwayApplication.java
@@ -2,8 +2,9 @@ package com.getyourway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class GetyourwayApplication {
 
     public static void main(String[] args) {
