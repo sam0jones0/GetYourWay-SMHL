54533521cb1594cd8d58ce554b68b5d3a0bb0bc4
Hilary Onoabhagbe <hilary.onoabhagbe@sky.uk>
2022-09-21 03:39:59 +0100

    Added basic security and User API

---
+++ src/main/java/com/getyourway/controller/MapController.java
@@ -0,0 +1,8 @@
+package com.getyourway.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MapController {
+    
+}
---
+++ src/main/java/com/getyourway/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.getyourway.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(Long id) {
+        super("Could not find user " + id);
+    }
+
+}
