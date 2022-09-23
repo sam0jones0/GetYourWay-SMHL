64113c67c1fe78a52c677d3cba0598322f02ddc5
mhas972 <matthew.adams2@sky.uk>
2022-09-23 16:56:22 +0100

    Add multi-day support for historical forecast

--- src/main/java/com/getyourway/GetyourwayApplication.java
+++ src/main/java/com/getyourway/GetyourwayApplication.java
@@ -11,5 +11,4 @@ public class GetyourwayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GetyourwayApplication.class, args);
     }
-
 }
--- src/main/java/com/getyourway/weather/WeatherController.java
+++ src/main/java/com/getyourway/weather/WeatherController.java
@@ -1,6 +1,5 @@
 package com.getyourway.weather;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.getyourway.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -8,13 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
-
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
 import java.time.*;
-import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/weather")
@@ -23,30 +19,34 @@ public class WeatherController {
     @Autowired
     private WeatherService weatherService;
 
-    @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<WeatherResponse> getWeatherLatLong(@RequestParam float lat, @RequestParam float lon) {
-        WeatherResponse response = weatherService.getWeatherByLatLong(lat, lon);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(response);
-    }
+//    @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<WeatherResponse> getWeatherLatLong(
+//            @RequestParam @DecimalMin(value=Constants.LAT_MIN, message="Latitude cannot be less than -90") @DecimalMax(value=Constants.LAT_MAX, message="Latitude cannot exceed 90") float lat,
+//            @RequestParam @DecimalMin(value=Constants.LON_MIN, message="Longitude cannot be less than -180") @DecimalMax(value=Constants.LON_MAX, message="Longitude cannot exceed than 180") float lon) {
+//        WeatherResponse response = weatherService.getWeatherByLatLong(lat, lon);
+//        return ResponseEntity
+//                .status(HttpStatus.OK)
+//                .body(response);
+//    }
 
-    @GetMapping("forecast")
-    public ResponseEntity<FullForecastResponse> getForecastLatLon(
-            @RequestParam @DecimalMin(Constants.LAT_MIN) @DecimalMax(Constants.LAT_MAX) float lat,
-            @RequestParam @DecimalMin(Constants.LON_MIN) @DecimalMax(Constants.LON_MAX) float lon) {
+    @GetMapping(value="forecast", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ForecastResponse> getForecastLatLon(
+            @RequestParam @DecimalMin(value=Constants.LAT_MIN, message="Latitude cannot be less than -90") @DecimalMax(value=Constants.LAT_MAX, message="Latitude cannot exceed 90") float lat,
+            @RequestParam @DecimalMin(value=Constants.LON_MIN, message="Longitude cannot be less than -180") @DecimalMax(value=Constants.LON_MAX, message="Longitude cannot exceed than 180") float lon) {
         var response = weatherService.getForecastByLatLon(lat, lon);
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(response);
     }
 
-    @GetMapping("historical")
+    @GetMapping(value="historical", consumes=MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Flux<String>> getHistoricalForecast(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
             @RequestParam int numDays,
-            @RequestParam @DecimalMin(Constants.LAT_MIN) @DecimalMax(Constants.LAT_MAX) float lat,
-            @RequestParam @DecimalMin(Constants.LON_MIN) @DecimalMax(Constants.LON_MAX) float lon) {
+            @RequestParam @DecimalMin(value=Constants.LAT_MIN, message="Latitude cannot be less than -90") @DecimalMax(value=Constants.LAT_MAX, message="Latitude cannot exceed 90") float lat,
+            @RequestParam @DecimalMin(value=Constants.LON_MIN, message="Longitude cannot be less than -180") @DecimalMax(value=Constants.LON_MAX, message="Longitude cannot exceed than 180") float lon) {
+
+        // Calculate each day in epoch seconds, create array for requests
         var epochSecond = date.toEpochDay() * Constants.SECONDS_IN_DAY;
         var days = new Long[numDays];
         for (int i = 0; i < days.length; i++) {
@@ -58,6 +58,5 @@ public class WeatherController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(response);
-
     }
 }
