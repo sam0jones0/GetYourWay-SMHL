08b17f0fde71fb381db6412ed67e0c58e97128d9
Sam Jones <sam0jones0@gmail.com>
2022-10-03 16:32:10 +0100

    Add some exception handling to FlightsController
    Also made further attempts at testing although progress is commented out for now.

--- src/main/java/com/getyourway/flights/FlightsController.java
+++ src/main/java/com/getyourway/flights/FlightsController.java
@@ -5,7 +5,6 @@ import com.getyourway.flights.localairportdb.InternalAirport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,8 +44,12 @@ public class FlightsController {
       @RequestParam @Size(min = 4, max = 4, message = "ICAO code must be exactly 4 characters")
           String arrIcao,
       @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate depDate) {
-    List<FlightDTO> response = flightsService.getFlightSchedule(depIcao, arrIcao, depDate);
-    return ResponseEntity.status(HttpStatus.OK).body(response);
+    try {
+      List<FlightDTO> response = flightsService.getFlightSchedule(depIcao, arrIcao, depDate);
+      return ResponseEntity.status(HttpStatus.OK).body(response);
+    } catch (AirportNotFoundException exception) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
   }
 
   /**
--- src/test/java/com/getyourway/flights/FlightServiceTests.java
+++ src/test/java/com/getyourway/flights/FlightServiceTests.java
@@ -1,75 +1,54 @@
-package com.getyourway.flights;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.getyourway.flights.localairportdb.InternalAirportRepo;
-import com.squareup.okhttp.mockwebserver.MockResponse;
-import com.squareup.okhttp.mockwebserver.MockWebServer;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-//@WebMvcTest
-@ExtendWith(MockitoExtension.class)
-@SpringBootTest(classes = FlightsService.class)
-@ActiveProfiles("flighttest")
-public class FlightServiceTests {
-
-  @MockBean
-  InternalAirportRepo airportRepo;
-
-  @InjectMocks
-  FlightsService flightsService = new FlightsService();
-
-  public static MockWebServer mockBackend;
-
-  @BeforeAll
-  static void setUp() throws IOException {
-    mockBackend = new MockWebServer();
-    mockBackend.start();
-  }
-
-  @AfterAll
-  static void tearDown() throws IOException {
-    mockBackend.shutdown();
-  }
-
-  @BeforeEach
-  void initialise() {
-    String baseUrl = String.format("https://aerodatabox.p.rapidapi.com:%s", mockBackend.getPort());
-  }
-
-  private String readJsonFromDiskResource(String filename) {
-    try (InputStream in =
-        Thread.currentThread().getContextClassLoader().getResourceAsStream(filename)) {
-      ObjectMapper mapper = new ObjectMapper();
-      JsonNode jsonNode = mapper.readValue(in, JsonNode.class);
-      return mapper.writeValueAsString(jsonNode);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  @Test
-  public void testGetAirportsNearby() {
-    String json = readJsonFromDiskResource("./getAirportsNearbyExternalResponse.json");
-    mockBackend.enqueue(new MockResponse().setBody(json));
-
-//    https://wiremock.org/docs/junit-jupiter/
+//package com.getyourway.flights;
 //
-//    AirportResponseDTO response = flightsService.getAirportsNearby((float) 51.4453, (float) -0.3375);
-//    System.out.println(response);
-
-//    mockBackend.enqueue();
-  }
-}
+//import com.fasterxml.jackson.databind.JsonNode;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.getyourway.flights.localairportdb.InternalAirportRepo;
+//import org.junit.Ignore;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.web.reactive.function.client.WebClient;
+//
+//import java.io.InputStream;
+//
+//@Ignore
+//@ExtendWith(MockitoExtension.class)
+//@SpringBootTest(classes = FlightsService.class)
+//@ActiveProfiles("flighttest")
+//public class FlightServiceTests {
+//
+//  @MockBean InternalAirportRepo airportRepo;
+//
+//  @MockBean WebClient webClient;
+//
+//  @InjectMocks FlightsService flightsService = new FlightsService();
+//
+//  private String readJsonFromDiskResource(String filename) {
+//    try (InputStream in =
+//        Thread.currentThread().getContextClassLoader().getResourceAsStream(filename)) {
+//      ObjectMapper mapper = new ObjectMapper();
+//      JsonNode jsonNode = mapper.readValue(in, JsonNode.class);
+//      return mapper.writeValueAsString(jsonNode);
+//    } catch (Exception e) {
+//      throw new RuntimeException(e);
+//    }
+//  }
+//
+////
+////  @Test
+////  public void testGetAirportsNearby() {
+////    String json = readJsonFromDiskResource("./getAirportsNearbyExternalResponse.json");
+////
+////    System.out.println(flightsService.getAirportsNearby((float) 50.3, (float) -0.3));
+////
+//    //    https://wiremock.org/docs/junit-jupiter/
+//    //
+//    //    AirportResponseDTO response = flightsService.getAirportsNearby((float) 51.4453, (float)
+//    // -0.3375);
+//    //    System.out.println(response);
+//
+//    //    mockBackend.enqueue();
+//  }
