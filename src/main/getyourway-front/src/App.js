a2eb5fbcf07a550627c067faafe6ec23f70c102b
Sam Jones <sam0jones0@gmail.com>
2022-10-06 12:14:02 +0100

    somechanges

--- src/main/getyourway-front/src/App.js
+++ src/main/getyourway-front/src/App.js
@@ -1,15 +1,16 @@
-
 import logo from "./logo.svg";
 import "./App.css";
 import { GoogleMap } from "@react-google-maps/api";
 
-import Header from './Components/Header'
+import Header from "./Components/Header";
 import Trip from "./Components/Trip/Trip.jsx";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h2>Hello</h2>
+
       <Trip />
     </div>
   );
