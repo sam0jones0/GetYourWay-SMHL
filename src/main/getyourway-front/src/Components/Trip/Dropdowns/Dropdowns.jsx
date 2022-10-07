599250c3990a6e9e35d682c4b06a4847c3cfe3f5
Sam Jones <sam0jones0@gmail.com>
2022-10-07 10:26:35 +0100

    Initial folder structure layout for dropdowns

---
+++ src/main/getyourway-front/src/Components/Trip/Dropdowns/Dropdowns.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+
+import FlightTimes from "./DropdownsChildComponents/FlightTimes";
+import WeatherForecast from "./DropdownsChildComponents/WeatherForecast";
+
+function Dropdowns() {
+  return (
+    <>
+      <FlightTimes />
+      <WeatherForecast />
+    </>
+  );
+}
---
+++ src/main/getyourway-front/src/Components/Trip/Dropdowns/DropdownsChildComponents/FlightTimes.jsx
@@ -0,0 +1,5 @@
+import React, { useEffect } from "react";
+
+function FlightTimes() {
+  return <></>;
+}
