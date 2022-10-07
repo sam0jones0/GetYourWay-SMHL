e982985c3a43cebf9d93f89c597be27cc7d9738c
Hilary Onoabhagbe <hilary.onoabhagbe@sky.uk>
2022-10-07 10:49:07 +0100

    Added map, directions, polyline and marker

--- src/main/getyourway-front/src/Components/Trip/Trip.jsx
+++ src/main/getyourway-front/src/Components/Trip/Trip.jsx
@@ -1,26 +1,37 @@
 import { useState } from "react";
 import "./Trip.css";
 import TripSubheader from "./TripSubheader/TripSubheader";
+import Map from "./Map/Map";
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
 export default function Trip() {
-  const [userLocation, setUserLocation] = useState([]);
+  const [userLocation, setUserLocation] = useState([51.5366, 0.0758]);
   const [nearbyAirports, setNearbyAirports] = useState({});
   const [departureAirport, setDepartureAirport] = useState({});
   const [destinationAirport, setDestinationAirport] = useState({});
   const [tripDate, setTripDate] = useState(new Date());
-
+  
   return (
-    <TripSubheader
-      userLocation={userLocation}
-      setUserLocation={setUserLocation}
-      nearbyAirports={nearbyAirports}
-      setNearbyAirports={setNearbyAirports}
-      departureAirportProp={departureAirport}
-      setDepartureAirport={setDepartureAirport}
-      destinationAirportProp={destinationAirport}
-      setDestinationAirport={setDestinationAirport}
-      tripDateProp={tripDate}
-      setTripDate={setTripDate}
-    />
+    <>
+      <TripSubheader
+        userLocation={userLocation}
+        setUserLocation={setUserLocation}
+        nearbyAirports={nearbyAirports}
+        setNearbyAirports={setNearbyAirports}
+        departureAirportProp={departureAirport}
+        setDepartureAirport={setDepartureAirport}
+        destinationAirportProp={destinationAirport}
+        setDestinationAirport={setDestinationAirport}
+        tripDateProp={tripDate}
+        setTripDate={setTripDate}
+      />
+      <Wrapper apiKey={"YOUR-API-KEY-HERE"}>
+        <Map 
+          location={userLocation}
+          departure={{lat: 51.8860, lng: 0.2389}} // London Stanstead Airport
+          destination={{lat: 49.0081, lng: 2.5509}} // Paris Charles de Gaulle Airport
+        />
+      </Wrapper>
+    </>
   );
 }
